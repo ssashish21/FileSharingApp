a15c7cacde709a743065f3180e711ee0ce553357
ssashish21 <ssashish21@gmail.com>
2021-10-06 19:41:51 +0530

    progress bar done

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const dropZone = document.querySelector(".drop-zone");
 const browseBtn = document.querySelector(".browseBtn");
 const fileInput = document.querySelector("#fileInput");
+const unsignedUploadPreset = 'nhmvf1es';
 
-const host = "https://github.com/codersgyan/inshare-file-sharing-app-api.git"
-const uploadURL = `${host}`;
+const progressContainer = document.querySelector(".progress-container");
+const bgProgress = document.querySelector(".bg-progress");
+const percentDiv = document.querySelector("#percent");
+const progressBar = document.querySelector(".progress-bar");
+
+const host = "https://api.cloudinary.com/v1_1/ssashish21/"
+const uploadURL = `${host}/upload`;
 // const uploadURL = `${host}api/files`;
-
 
 dropZone.addEventListener("dragover" , (e) =>{
     e.preventDefault()
@@ -22,6 +27,7 @@ dropZone.addEventListener("dragleave" , ()=>{
 dropZone.addEventListener("drop" , (e)=>{
     e.preventDefault();
     dropZone.classList.remove("dragged");
+    
     const files = e.dataTransfer.files;
     console.log(files);
     if(files.length){
@@ -39,26 +45,39 @@ browseBtn.addEventListener("click" , ()=>{
 });
 
 const uploadFile = ()=>{
+    progressContainer.style.display = "block";
     const file = fileInput.files[0];
-    const formData = new FormData();
-    formData.append("myfile" , file);
-
-
     const xhr = new XMLHttpRequest();
+    const formData = new FormData();
+    xhr.open("POST" , uploadURL , true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 
-    xhr.onreadystatechange = ()=>{
-        if(xhr.readyState == XMLHttpRequest.DONE){
-            console.log(xhr.response);
-        }
-    };
 
     xhr.upload.onprogress = updateProgress;
 
-    xhr.open("POST" , uploadURL);
+
+    //while changing states
+    xhr.onreadystatechange = ()=>{
+        if(xhr.readyState == XMLHttpRequest.DONE){
+            // console.log(xhr.response);
+            showLink(JSON.parse(xhr.response));
+        }
+    };
+    formData.append('upload_preset', unsignedUploadPreset);
+    formData.append("file" , file);
+
     xhr.send(formData);
 };
 
 const updateProgress = (e)=> {
     const percent = Math.round((e.loaded / e.total) * 100);
-    console.log(percent);
+    // console.log(percent);
+    bgProgress.style.width = `${percent}%`;
+    percentDiv.innerText = percent;
+    progressBar.style.transform = `scaleX(${percent/100})`;
+};
+
+const showLink = (res)=>{
+    console.log(res.secure_url); // display link
+    progressContainer.style.display = "none";
 }
